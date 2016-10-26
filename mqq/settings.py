9f94ef461ea8a496d92e051a55dfe48444bc4c46
pafou <pfoucher@gmail.com>
2016-10-26 22:34:48 +0200

    0.6: settings.py: INSTALLED_APPS added: rest_framework

--- mqq/settings.py
+++ mqq/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',
+    'rest_framework',
+    'mqqs',
+    'mqq'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +104,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}

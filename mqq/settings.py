fc1a3b17e6016db6b60ab2f48fca8433ddb715d1
pafou <pfoucher@gmail.com>
2016-10-27 20:28:59 +0200

    0.20: debug again

--- mqq/settings.py
+++ mqq/settings.py
@@ -112,15 +112,3 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-#--
-
-TEMPLATE_DIRS = [
-    os.path.join(BASE_DIR, 'templates'),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static', 'dirs'),
-)
-

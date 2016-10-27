aecf81d502bc1d34a5580e368284b814c90b7110
pafou <pfoucher@gmail.com>
2016-10-27 08:00:39 +0200

    0.14:swag is in the air

--- mqqs/urls.py
+++ mqqs/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-#from rest_framework_swagger.views import get_swagger_view
+from rest_framework_swagger.views import get_swagger_view
 
 from mqqs import views
 
-#schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
     url(r'^$', views.MqqList.as_view()),
     url(r'^(?P<pk>[0-9]+)/$', views.MqqDetail.as_view()),
-#    url(r'^doc$', schema_view)
+    url(r'^doc$', schema_view)
 
 ]
 

315067c392d842818d06066ca74fdbb591c079e6
pafou <pfoucher@gmail.com>
2016-11-08 20:06:27 +0100

    0.26 : version avec MqqAdmin dans admin.py

--- mqqs/admin.py
+++ mqqs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from mqqs.models import Mqq
 
-# Register your models here.
+class MqqAdmin(admin.ModelAdmin):
+    list_display   = ('alias', 'env', 'type', 'deep')
+    list_filter    = ('alias','env',)
+    ordering       = ('alias', )
+    search_fields  = ('alias', 'env')
+    
+admin.site.register(Mqq, MqqAdmin)
